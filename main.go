8112cf419448aaf9786e21bd634b1ffbce423ead
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-01-09 13:11:18 +0530

    running cli and returning tokens

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
-package main 
+package main
 
-func main(){
-	
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to Cute Line Interface")
+
+	for {
+		scanner.Scan()
+		text := scanner.Text()
+
+		out := tokenise(text)
+		fmt.Print(">")
+
+		for i := 0; i < len(out); i++ {
+			fmt.Print(out[i], " ")
+		}
+		fmt.Println()
+	}
+
 }
---
+++ repl.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"strings"
+)
+
+func tokenise(text string) (output []string) {
+	lowered_text := strings.ToLower(text)
+	output = strings.Fields(lowered_text)
+	return output
+}
