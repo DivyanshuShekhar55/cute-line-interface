3b382fefc4f6aaa5ea90e2c3d0f3555e82264ab1
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-01-17 11:45:56 +0530

    render first line added

--- monkey/table-renderer.go
+++ monkey/table-renderer.go
@@ -3,7 +3,28 @@ package monkey
 import (
 	"cute-line-interface/utils"
 	"errors"
+	"strings"
 )
+
+func renderFirstLine(widths []int, styles style) {
+	
+	var finalStr string
+	finalStr += "\U0000250C"                              // ┌
+	finalStr += strings.Repeat("\U00002500", widths[0]-1) // -
+	finalStr += "\U0000252C"                              // ┬
+	finalStr += strings.Repeat("\U00002500", widths[1]-1) // -
+	finalStr += "\U0000252C"                              // ┬
+	finalStr += strings.Repeat("\U00002500", widths[2]-1) // -
+	finalStr += "\U00002510"
+	//  ┐
+	
+	utils.TurnText(finalStr, styles.tableCol, true, false)
+
+}
+
+func renderMiddleRows(t *Table) {}
+
+func renderLastLine(t *Table) {}
 
 func (t *Table) Render() {
 	if t.header == nil {
@@ -12,6 +33,8 @@ func (t *Table) Render() {
 		return
 	}
 
-	
+	//widths := countTotalColChars(t)
+
+	// print first line
 
 }
