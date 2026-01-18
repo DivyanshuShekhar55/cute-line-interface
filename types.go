8b1c5c6031089ac93cdc71ece8e0f98ce71ed0a8
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-01-18 13:06:52 +0530

    added types file

---
+++ types.go
@@ -0,0 +1,50 @@
+package main
+
+import "cute-line-interface/httpx"
+
+type Terminal struct {
+	HttpClient *httpx.Client
+}
+
+type cmd struct {
+	name     string
+	desc     string
+	callback func()
+}
+
+type Company struct {
+	Name        string `json:"name"`
+	CatchPhrase string `json:"catchPhrase"`
+	Bs          string `json:"bs"`
+}
+
+type Geo struct {
+	Lat string `json:"lat"`
+	Lng string `json:"lng"`
+}
+
+type Address struct {
+	Street  string `json:"street"`
+	Suite   string `json:"suite"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+	Geo     Geo    `json:"geo"`
+}
+
+type User struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Username string  `json:"username"`
+	Email    string  `json:"email"`
+	Address  Address `json:"address"`
+	Phone    string  `json:"phone"`
+	Website  string  `json:"website"`
+	Company  Company `json:"company"`
+}
+
+type _User struct {
+	name     string
+	username string
+	email    string
+	website  string
+}
